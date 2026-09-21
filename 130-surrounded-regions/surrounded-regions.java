acbb088c750507d2f6dbc96b816c987c9c3f7c08
Thathika <thathikam.cs24@bitsathy.ac.in>
2026-09-21 17:20:05 +0530

    Time: 1 ms (99.81%) | Memory: 47.9 MB (22.39%) - LeetSync

---
+++ 130-surrounded-regions/surrounded-regions.java
@@ -0,0 +1,45 @@
+class Solution {
+    public void solve(char[][] board) {
+        int m = board.length;
+        int n = board[0].length;
+
+        // Check first and last column
+        for (int i = 0; i < m; i++) {
+            dfs(board, i, 0);
+            dfs(board, i, n - 1);
+        }
+
+        // Check first and last row
+        for (int j = 0; j < n; j++) {
+            dfs(board, 0, j);
+            dfs(board, m - 1, j);
+        }
+
+        // Convert surrounded O's to X's
+        // Convert safe O's back to O's
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == 'O') {
+                    board[i][j] = 'X';
+                } else if (board[i][j] == '#') {
+                    board[i][j] = 'O';
+                }
+            }
+        }
+    }
+
+    public void dfs(char[][] board, int i, int j) {
+        if (i < 0 || i >= board.length ||
+            j < 0 || j >= board[0].length ||
+            board[i][j] != 'O') {
+            return;
+        }
+
+        board[i][j] = '#';
+
+        dfs(board, i + 1, j);
+        dfs(board, i - 1, j);
+        dfs(board, i, j + 1);
+        dfs(board, i, j - 1);
+    }
+}
